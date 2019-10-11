0aeeab1d279cf1bec4aea9ba4f71692aaff13f6b
7audio <7audio@gmail.com>
2019-10-11 16:51:26 +0300

    Initial game

---
+++ GameComposition.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container" @mouseup="stopDrag">
+    <div class="bird" @mousedown="startDrag" :style="birdPosition"/>
+    <div class="target" :style="targetPosition"/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+import { reactive, watch, computed } from '@vue/composition-api'
+
+Vue.use(VueCompositionApi)
+
+function useCatapult(moveBird) {
+  const dragStart = reactive({ x: null, y: null })
+  const startDrag = e => {
+    dragStart.x = e.clientX
+    dragStart.y = 720 - e.clientY
+  }
+  const stopDrag = e => {
+    moveBird(calculateTrajectory(dragStart.x, dragStart.y, e.clientX, 720 - e.clientY))
+  }
+  const calculateTrajectory = (x1, y1, x2, y2) => {
+    let Vx = (x1 - x2)/1.7,
+      Vy = (y1 - y2)/1.7,
+      result = []
+    for (let t = 0; t < 30; t++) {
+      result.push({
+        y: Vy * t - 9.8 * t * t/2 + y1,
+        x: x1 + Vx * t
+      })
+    }
+    return result
+  }
+  return {
+    startDrag,
+    stopDrag
+  }
+}
+
+function useBird() {
+  const initialBird = () => ({ x: 229, y: 256 })
+  const bird = reactive(initialBird())
+  const moveBird = trajectory => {
+    trajectory.map(({ x, y }, t) => setTimeout(() => {
+      bird.x = x
+      bird.y = y
+    }, t * 65))
+    setTimeout(resetBird, 2000)
+  }
+  const resetBird = () => {
+    bird.x = initialBird().x
+    bird.y = initialBird().y
+  }
+  const birdPosition = computed(() => ({
+    left: bird.x + 'px',
+    bottom: bird.y + 'px'
+  }))
+  return {
+    bird,
+    moveBird,
+    birdPosition
+  }
+}
+
+function useTarget(bird) {
+  const target = { x: 1000, y: 263 }
+  const targetPosition = computed(() => ({
+    left: target.x + 'px',
+    bottom: target.y + 'px'
+  }))
+  watch(() => {
+    if (bird.x >= target.x + 25 && bird.x <= target.x + 152 && bird.y >= target.y - 45 && bird.y <= target.y + 132) {
+      alert("you win!")
+    }
+  })
+  return { targetPosition }
+}
+
+export default {
+  setup() {
+    const { bird, moveBird, birdPosition } = useBird()
+    return {
+      birdPosition,
+      ...useCatapult(moveBird),
+      ...useTarget(bird)
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/style.css';
+</style>
---
+++ GameOld.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container" @mouseup="stopDrag">
+    <div class="bird" @mousedown="startDrag" :style="birdPosition"/>
+    <div class="target" :style="targetPosition"/>
+  </div>
+</template>
+
+<script>
+const initialBird = () => ({ x: 229, y: 256 })
+const target = { x: 1000, y: 263 }
+
+export default {
+  data() {
+    return {
+      dragStart: { x: null, y: null },
+      bird: initialBird()
+    }
+  },
+  methods: {
+    startDrag(e) {
+      this.dragStart.x = e.clientX
+      this.dragStart.y = 720 - e.clientY
+    },
+    stopDrag(e) {
+      this.moveBird(this.calculateTrajectory(this.dragStart.x, this.dragStart.y, e.clientX, 720 - e.clientY))
+    },
+    calculateTrajectory(x1, y1, x2, y2) {
+      let Vx = (x1 - x2)/1.7,
+        Vy = (y1 - y2)/1.7,
+        result = []
+      for (let t = 0; t < 30; t++) {
+        result.push({
+          y: Vy * t - 9.8 * t * t/2 + y1,
+          x: x1 + Vx * t
+        })
+      }
+      return result
+    },
+    moveBird(trajectory) {
+      trajectory.map((position, t) => setTimeout(() => this.bird = position, t * 65))
+      setTimeout(this.resetBird, 2000)
+    },
+    resetBird() {
+      this.bird = initialBird()
+    }
+  },
+  computed: {
+    birdPosition() {
+      return {
+        left: this.bird.x + 'px',
+        bottom: this.bird.y + 'px'
+      }
+    },
+    targetPosition() {
+      return {
+        left: target.x + 'px',
+        bottom: target.y + 'px'
+      }
+    }
+  },
+  watch: {
+    bird: {
+      handler (val) {
+        if (val.x >= target.x + 25 && val.x <= target.x + 152 && val.y >= target.y - 45 && val.y <= target.y + 132) {
+          alert("you win!")
+          this.resetBird()
+        }
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/style.css';
+</style>
