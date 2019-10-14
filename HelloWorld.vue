dc472cef2672418ca8307a2f0ddcf9f74e20d5a0
7audio <7audio@gmail.com>
2019-10-14 23:16:38 +0300

    Add simplified HelloWorld from documentation

---
+++ HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>count is {{ count }}</p>
+    <p>plusOne is {{ plusOne }}</p>
+    <button @click="increment">count++</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { ref, computed, watch } from '@vue/composition-api'
+import VueCompositionApi from '@vue/composition-api'
+
+Vue.use(VueCompositionApi)
+
+export default {
+  setup() {
+    const count = ref(0)
+    const plusOne = computed(() => count.value + 1)
+    const increment = () => { count.value++ }
+    // eslint-disable-next-line
+    watch(() => count.value * 2, val => console.log(`count * 2 is ${val}`))
+    return {
+      count,
+      plusOne,
+      increment
+    }
+  }
+}
+</script>
